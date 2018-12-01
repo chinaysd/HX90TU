1cbf74135b0911d614cc71ed2b513609b0e4b3be
ysd <ysd@emomo.com>
2018-12-01 14:47:50 +0800

    [master]增加了红灯指示

--- HX90TU_TOUCH_KEY/user/system.c
+++ HX90TU_TOUCH_KEY/user/system.c
@@ -15,17 +15,39 @@ void System_Init(void)
 	Line_Status(Line2,Line2_Hight);
 }
 
+void Delay_Time(unsigned int Time)
+{
+    static unsigned int i,j;
+    for(i = Time;i > 0 ; i --)
+    {
+        for(j = 200; j > 0 ;j --);
+    }
+}
+
 void System_Handle(void)
 {
+    #if 0
+    static unsigned int Cnt;
+    ++ Cnt;
+    if(Cnt & 0x01)
+    {
+       Line_Status(Line1,Line1_Hight); 
+       Delay_Time(1000);
+    }
+    else 
+    {
+       Line_Status(Line1,Line1_Low);
+       Delay_Time(1000);        
+    }
+    #endif
 	if(SOCAPI_TouchKeyStatus&0x80)
 	{
 		SOCAPI_TouchKeyStatus &=0x7f;  // 重要步骤  3:  清除标志位， 需要外部清除。
-    My_KeyValue = TouchKeyScan();
+        My_KeyValue = TouchKeyScan();
 		switch (My_KeyValue)
 		{
 			case 0x0002:
-				          //Line_Status(Line1,Line1_Hight);
-			             Line_Status(Line2,Line2_Low);
+			              Line_Status(Line2,Line2_Low);
 				          break;
 			case 0x0040:
 				          Line_Status(Line2,Line2_Low);
